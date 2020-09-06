0a71f337a0a8b7d7517b5924913e0b6cf8657ec0
rvcabc <brycejcorbin@gmail.com>
2020-09-06 11:07:55 -0700

    base setup

---
+++ profiledef.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="clarch"
+iso_label="CLARCH_$(date +%y%m)"
+iso_publisher="Bryce Corbin <website down>"
+iso_application="Arch Linux Live/Rescue CD"
+iso_version="$(date +%y.%m.%d)"
+install_dir="arch"
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
